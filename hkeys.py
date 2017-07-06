e715048b1969bd46ef9ad5f3bf49c40c353083c8
springdom <taylomj5@gmail.com>
2017-07-06 14:11:29 -0400

    Update

--- hkeys.py
+++ hkeys.py
@@ -11,14 +11,17 @@ from subprocess import Popen
 print("Select From Menu")
 print("1: Lead Package Mktg Package Entry & Edit")
 print("2: Unlock Lead")
+print("3: Change to Kick")
 print("Already On Page?")
-print("3: Book Dates")
-print("4: Unlock Lead")
+print("4: Book Dates")
+print("5: Unlock Lead")
 
 Sel = int(input("Select Option: "))
 
 opt1 = [1,2,1,2]
 opt2 = [1,2,1,15,4,8]
+opt3 = [1,2,1,15,4,10]
+
 
 lead = input("Enter Lead: ")
 loc = input("Enter Location: ")
@@ -46,12 +49,41 @@ def unlock_lead(leadn,loca):
 
 #Lead Mktg Package Entry & Edit
 def lead_package(leadn,loca):
+    
     SendKeys(leadn + '{ENTER}')
     SendKeys(loca + '{ENTER}')
     SendKeys('{ENTER 2}')
+"""
+    package_sel = int(input("Select Package Number: "))
+    prop = int(input("Enter Property Number: "))
+    unit_type int(input("Unit Type: "))
+    adults = int(input("How many adults: "))
+    children = int(input("How many children"))
+    arrival = input("Arrival: ")
+    nights = input("Nights: ")
+    availability = 'n'
+    backspace = 'n'
+""" 
     #Ask Which Package
     #SK-7,SK-A
     #1stSK-PropName,
+
+def tour_booking():
+    office = 0
+    
+def change_to_kick(leadn,loca):
+    SendKeys(leadn + '{ENTER}')
+    SendKeys(loca + '{ENTER}')
+    SendKeys('{ENTER 2}')
+
+    package_sel = input("Enter Package Number: ")
+    SendKeys(package_sel)
+    SendKeys("2")
+    SendKeys("{ENTER}")
+    SendKeys("9")
+    SendKeys("{ENTER 2}")
+    SendKeys("f")
+    
 
 print(Sel)
 if Sel == 1:
@@ -59,11 +91,13 @@ if Sel == 1:
 elif Sel == 2:
     mode(opt2)
 elif Sel == 3:
-    lead_package(lead,loc)
+    mode(opt3)
 elif Sel == 4:
+    lead_package(lead,loc)
+elif Sel == 5:
     unlock_lead(lead,loc)
 
-Book Dates
+###Book Dates
     #Recent Date Opt
     #Opt 7, Opt A
     #Property
