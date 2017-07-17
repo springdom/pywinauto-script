c2e19819566c74ce5b17f5624ad8722dad2a8c9d
Matthew <taylomj5@gmail.com>
2017-07-17 15:38:16 -0400

    Updated

--- hkeys.py
+++ hkeys.py
@@ -7,49 +7,45 @@ from pywinauto.application import Application
 from pywinauto.findwindows import find_window
 from subprocess import Popen
 
-#Display Modes
 #app.dlg.print_control_identifiers() #Check Identifiers
 opt1 = [1,2,1,2]
 opt2 = [1,2,1,15,4,8]
 opt3 = [1,2,1,15,4,10]
 
 def main():
+    print("------------------")
     print("Select From Menu")
     print("0: Check Availability")
     print("9: Check Tours")
-    print("1: Lead Package Mktg Package Entry & Edit")
+    print("------------------")
+    print("1: Book Dates")
     print("2: Unlock Lead")
+    print("------------------")
     print("3: Change to Kick")
-    print("Already On Page?")
-    print("4: Book Dates")
-    print("5: Unlock Lead")
-    print("6: Kick Package")
-    print("")
+    print("4: Kick Package")
+    print("------------------")
     Sel = int(input("Select Option: "))
     print("")
 
-    if Sel == 1:
-        mode(opt1)
-    elif Sel == 2:
-        mode(opt2)
-    elif Sel == 3:
+    if Sel >= 1 and Sel <= 4:
         lead = input("Enter Lead: ")
         loc = input("Enter Location: ")
+
+    if Sel == 1:
+        lead_package(lead,loc)
+    elif Sel == 2:
+        unlock_lead(lead,loc)
+    elif Sel == 3:
         change_to_kick(lead,loc)
     elif Sel == 4:
-        lead = input("Enter Lead: ")
-        loc = input("Enter Location: ")
-        lead_package(lead,loc)
-    elif Sel == 5:
-        unlock_lead(lead,loc)
-    elif Sel == 6:
-        lead = input("Enter Lead: ")
-        loc = input("Enter Location: ")
         kick_package(lead,loc)
     elif Sel == 0:
         check_dates()
     elif Sel == 9:
         check_tours()
+    else:
+        print("Invalid Option")
+        main()
 
 
 #Window Settings
@@ -64,7 +60,7 @@ def set_window():
 #Enter Mode
 def mode(opt):
     set_window()
-    SendKeys('{BACKSPACE 10}')
+    SendKeys('{BACKSPACE 8}')
     SendKeys('{ENTER}')
     time.sleep(1)
     
@@ -73,15 +69,19 @@ def mode(opt):
         SendKeys('{ENTER}')
     SendKeys('{ENTER}')
 
+#Unlock Lead
 def unlock_lead(leadn,loca):
+    mode(opt2)
     set_window()
     SendKeys(leadn + '{ENTER}')
     SendKeys(loca + '{ENTER}')
     SendKeys('{ENTER 4}')
+    main()
 
 #Lead Mktg Package Entry & Edit
 def lead_package(leadn,loca):
-    set_window()
+    mode(opt1)
+    
     SendKeys(leadn + '{ENTER}')
     SendKeys(loca + '{ENTER}')
     SendKeys('{ENTER 2}')
@@ -110,7 +110,7 @@ def lead_package(leadn,loca):
     SendKeys(arrival + '{ENTER}')
     SendKeys(nights + '{ENTER 2}')
     
-    bckspace = input("Go back? y or n:") # Backspace
+    bckspace = input("Go back? y or n: ") # Backspace
 
     set_window()
 
@@ -123,7 +123,7 @@ def lead_package(leadn,loca):
     
     if proceed == 'y':
         SendKeys('{ENTER}')
-    #Enter
+
     time.sleep(1)  
     office = input("Enter Office: ")
     manifest = input("Manifest: ")
@@ -134,9 +134,16 @@ def lead_package(leadn,loca):
     set_window()
     if toursa == "y":
         SendKeys('1' + '{ENTER 2}')
-        SendKeys(manifest + '{ENTER 8}')
-        
-
+        SendKeys(manifest)
+        SendKeys('{ENTER 14}')
+        SendKeys('f')
+        SendKeys('y')
+        amount = input("Enter amount: ")
+        set_window()
+        SendKeys(amount)
+    else:
+        pass
+    
 def check_avail():
     region = input("Enter Region: ")
     arrival = input("Enter Arrival: ")
@@ -150,9 +157,7 @@ def check_avail():
     SendKeys('{TAB}' + '3' + '{ENTER}')
     main()
 
-def tour_booking():
-    office = 0
-
+#Kick Package
 def kick_package(leadn,loca):
     mode(opt1)
     
@@ -167,15 +172,17 @@ def kick_package(leadn,loca):
     SendKeys(package_sel + '{ENTER}')
     SendKeys('20' + '{ENTER}')
     SendKeys('2' + '{ENTER}')
-    SendKeys('14')
-    SendKeys('{ENTER}')
-    SendKeys('{TAB}' + '{ENTER 8}')
+    SendKeys('14' + '{ENTER}')
+    SendKeys('{TAB}')
+    time.sleep(0.50)
+    SendKeys('{ENTER 8}')
     SendKeys('13')
-    SendKeys('A')
-    SendKeys('10' + '{ENTER 5}')
-    SendKeys('Package Kicked as requested')
+    #SendKeys('A')
+    #SendKeys('10' + '{ENTER 5}')
+    #SendKeys('Package Kicked as requested')
+    main()
     
-    
+#Change To Kick
 def change_to_kick(leadn,loca):
     mode(opt3)
    
@@ -192,8 +199,9 @@ def change_to_kick(leadn,loca):
     SendKeys("9")
     SendKeys("{ENTER 2}")
     SendKeys("f")
+    main()
     
-
+#Check Dates
 def check_dates():
     region = input("Enter Region: ")
     arrival = input("Enter Arrival: ")
@@ -203,9 +211,9 @@ def check_dates():
     SendKeys(region + '{ENTER 2}')
     SendKeys(arrival + '{ENTER}')
     SendKeys('{TAB}' + '2' + '{ENTER}')
-    nory = input("Return To Options: y or n")
     main()
 
+#Check Tours
 def check_tours():
     office = input("Enter Office: ")
     starting = input("Enter Tour Date: ")
@@ -216,5 +224,8 @@ def check_tours():
     SendKeys(starting + '{ENTER 3}')
     SendKeys('{TAB}' + '2' + '{ENTER}')
     main()
+
+def check_cashback():
+    pass
 
 main()
