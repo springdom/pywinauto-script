c644b97a7c683a566ac9566f00fe3a64404a7b23
Matthew <taylomj5@gmail.com>
2017-08-01 22:02:22 -0400

    aa

--- excel.py
+++ excel.py
@@ -3,8 +3,19 @@ from openpyxl import load_workbook
 wb = load_workbook('KeyCodes.xlsx', data_only=True)
 sh=wb["Sheet1"]
 ws = wb.active
+num = 0
 test = []
-for row in ws.iter_rows(min_row=18, max_col=33, max_row=18):
+loc_dict = {}
+
+#KeyCodes - Outbound, Orlando, Las Vegas, Gold Mountain
+
+for row in ws.iter_rows(min_row=18, max_col=33, max_row=27):
     for cell in row:
-        test.append(cell.value)
-        print(cell.value)
+        num += 1
+        if num == 1:
+            #print(cell.value)
+            loc_dict[cell.value] = None
+        if num == 33:
+            num = 0
+            #print(cell.value)
+            
