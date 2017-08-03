0833c1cb357365edbbf953465d26a0eef2929831
Matthew <taylomj5@gmail.com>
2017-08-02 21:23:08 -0400

    Update excel.py

--- excel.py
+++ excel.py
@@ -3,19 +3,32 @@ from openpyxl import load_workbook
 wb = load_workbook('KeyCodes.xlsx', data_only=True)
 sh=wb["Sheet1"]
 ws = wb.active
-num = 0
-test = []
-loc_dict = {}
 
-#KeyCodes - Outbound, Orlando, Las Vegas, Gold Mountain
+outbnd = {}
+orl_call_trsf = {}
+lvn_call_trsf = {}
+gldmtn_call_trsf = {}
 
-for row in ws.iter_rows(min_row=18, max_col=33, max_row=27):
-    for cell in row:
-        num += 1
-        if num == 1:
-            #print(cell.value)
-            loc_dict[cell.value] = None
-        if num == 33:
-            num = 0
-            #print(cell.value)
-            
+
+
+def keycodes(m_row,mx_col,mx_row):
+    num = 0
+    #KeyCodes - Outbound, Orlando, Las Vegas, Gold Mountain
+    for row in ws.iter_rows(min_row=m_row, max_col=mx_col, max_row=mx_row):
+        for cell in row:
+            num += 1
+            if num == 1:
+                #print(cell.value)
+                orl_call_trsf[cell.value] = None
+            if num == 33:
+                num = 0
+
+def assign_key(x):
+    n = 18
+    for k,v in x.items():
+        x[k] = sh['D%s' % n].value
+        n += 1
+
+keycodes(18,33,27)
+assign_key(orl_call_trsf)
+print(orl_call_trsf)
