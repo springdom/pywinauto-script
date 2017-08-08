4a77b522ed6f6bc7a1656ba0afcb5a6fa231aeed
springdom <springdom@github.com>
2017-08-08 11:16:42 -0400

    cleanup with pylint

--- hkeys.py
+++ hkeys.py
@@ -1,20 +1,20 @@
 import time
-import win32clipboard
 import pywinauto
-from pywinauto import application
 from pywinauto.keyboard import SendKeys
-from pywinauto import Desktop
 from pywinauto.application import Application
-from pywinauto.findwindows import find_window
 from openpyxl import load_workbook
+#from pywinauto import application
+#import win32clipboard
+#from pywinauto import Desktop
+#from pywinauto.findwindows import find_window
 
 #app.dlg.print_control_identifiers() #Check Identifiers
 wb = load_workbook('KeyCodes.xlsx', data_only=True)
-sh=wb["Sheet1"]
+sh = wb["Sheet1"]
 ws = wb.active
 
 wb2 = load_workbook('changetokick.xlsx', data_only=True)
-sh2=wb["Sheet1"]
+sh2 = wb["Sheet1"]
 ws2 = wb2.active
 
 outbnd_orl = {}
@@ -22,9 +22,9 @@ orl_call_trsf = {}
 lvn_call_trsf = {}
 gldmtn_call_trsf = {}
 
-opt1 = [1,2,1,2]
-opt2 = [1,2,1,15,4,8]
-opt3 = [1,2,1,15,4,10]
+opt1 = [1, 2, 1, 2]
+opt2 = [1, 2, 1, 15, 4, 8]
+opt3 = [1, 2, 1, 15, 4, 10]
 
 def main():
     print("-" * 20)
@@ -55,13 +55,13 @@ def main():
         check_avail()
         checkavail = input("Available: y or n: ")
         if checkavail == "y":
-            lead_package(lead,loc)
+            lead_package(lead, loc)
         else:
             main()
     elif Sel == 2:
-        unlock_lead(lead,loc)
+        unlock_lead(lead, loc)
     elif Sel == 3:
-        kick_package(lead,loc)
+        kick_package(lead, loc)
     elif Sel == 4:
         """
         win32clipboard.OpenClipboard()
@@ -70,7 +70,7 @@ def main():
         leadvar = data.split("-")
         change_to_kick(leadvar[1],leadvar[0])
         """
-        kick_list(3,1,56)
+        kick_list(3, 1, 56)
     elif Sel == 5:
         build_package()
     elif Sel == 0:
@@ -81,7 +81,7 @@ def main():
         check_avail()
         main()
     elif Sel == 7:
-        check_cashback(lead,loc)
+        check_cashback(lead, loc)
     else:
         print("Invalid Option")
         main()
@@ -101,14 +101,14 @@ def mode(opt):
     SendKeys('{BACKSPACE 10}')
     SendKeys('{ENTER}')
     time.sleep(1.5)
-    
+
     for num in opt:
         SendKeys(str(num))
         SendKeys('{ENTER}')
     SendKeys('{ENTER}')
 
 #KeyCodes - Outbound, Orlando, Las Vegas, Gold Mountain
-def keycodes(m_row,mx_col,mx_row,x):
+def keycodes(m_row, mx_col, mx_row, x):
     num = 0
     for row in ws.iter_rows(min_row=m_row, max_col=mx_col, max_row=mx_row):
         for cell in row:
@@ -122,40 +122,37 @@ def keycodes(m_row,mx_col,mx_row,x):
             else:
                 if num == 3:
                     num = 0
-def assign_key(n,x):
+
+def assign_key(n, x):
     if n > 17:
-        for k,v in x.items():
+        for k, v in x.items():
             x[k] = sh['D%s' % n].value
             n += 1
     else:
-        for k,v in x.items():
+        for k, v in x.items():
             x[k] = sh['B%s' % n].value
             n += 1
 
-def kick_list(m_row,mx_col,mx_row):
+def kick_list(m_row, mx_col, mx_row):
     for row in ws2.iter_rows(min_row=m_row, max_col=mx_col, max_row=mx_row):
         for cell in row:
-                lead = cell.value.split("-")
-                change_to_kick(lead[1],lead[0])
-
+            lead = cell.value.split("-")
+            change_to_kick(lead[1], lead[0])
 
 #Lead Mktg Package Entry & Edit
-def lead_package(leadn,loca):
+def lead_package(leadn, loca):
     mode(opt1)
-    
     SendKeys(leadn + '{ENTER}')
     SendKeys(loca + '{ENTER}')
     SendKeys('{ENTER 2}')
     try:
         package_sel = input("Select Package Number: ")
-        
+
         set_window()
-        
+
         SendKeys('{ENTER}')
         SendKeys(package_sel + '{ENTER}')
         SendKeys('7' + '{ENTER}' + 'A' + '{ENTER}')
-
-    
         propn = input("Enter Property Number: ")
         unitt = input("Enter Unit Type: ")
         adults = input("Adults: ")
@@ -164,14 +161,13 @@ def lead_package(leadn,loca):
         nights = input("Nights: ")
 
         set_window()
-    
+
         SendKeys(propn + '{ENTER 2}')
         SendKeys(unitt + '{ENTER 4}')
         SendKeys(adults + '{ENTER 2}')
         SendKeys(kids + '{ENTER 2}')
         SendKeys(arrival + '{ENTER}')
         SendKeys(nights + '{ENTER 2}')
-    
         bckspace = input("Go back? y or n: ") # Backspace
 
         set_window()
@@ -182,12 +178,12 @@ def lead_package(leadn,loca):
         proceed = input("Proceed - y or n: ")
 
         set_window()
-    
+
         if proceed == 'y':
             SendKeys('{ENTER}')
 
-        
-        time.sleep(1)  
+
+        time.sleep(1)
         office = input("Enter Office: ")
         manifest = input("Manifest: ")
         set_window()
@@ -195,7 +191,7 @@ def lead_package(leadn,loca):
         toursa = input("Tours Available y or n: ")
 
         set_window()
-    
+
         if toursa == "y":
             SendKeys('1' + '{ENTER 2}')
             SendKeys(manifest)
@@ -215,7 +211,7 @@ def lead_package(leadn,loca):
         main()
 
 #Unlock Lead
-def unlock_lead(leadn,loca):
+def unlock_lead(leadn, loca):
     mode(opt2)
     set_window()
     SendKeys(leadn + '{ENTER}')
@@ -224,16 +220,16 @@ def unlock_lead(leadn,loca):
     main()
 
 #Kick Package
-def kick_package(leadn,loca):
+def kick_package(leadn, loca):
     mode(opt1)
     txtcmnt = "Package Kicked as requested"
     SendKeys(leadn + '{ENTER}')
     SendKeys(loca + '{ENTER}')
     SendKeys('{ENTER 2}')
     package_sel = input("Select Package Number: ")
-    
+
     set_window()
-    
+
     SendKeys('{ENTER}')
     SendKeys(package_sel + '{ENTER}')
     SendKeys('20' + '{ENTER}')
@@ -247,18 +243,18 @@ def kick_package(leadn,loca):
     #SendKeys('10' + '{ENTER 5}')
     #SendKeys(txtcmnt.replace(" ","{SPACE}") + "{ENTER}")
     main()
-    
+
 #Change To Kick
 def change_to_kick(leadn, loca):
     mode(opt3)
-   
+
     SendKeys(leadn + '{ENTER}')
     SendKeys(loca + '{ENTER}')
     SendKeys('{ENTER 2}')
 
     package_sel = input("Enter Package Number: ")
     set_window()
-    
+
     SendKeys(package_sel + '{ENTER}')
     SendKeys("2")
     SendKeys("{ENTER}")
@@ -274,6 +270,7 @@ def check_avail():
     nights = input("Enter Nights: ")
 
     mode(opt1)
+
     SendKeys('{TAB}' + '2' + '{ENTER}')
     SendKeys(region + '{ENTER 2}')
     SendKeys(arrival + '{ENTER 2}')
@@ -285,8 +282,9 @@ def check_avail():
 def check_dates():
     region = input("Enter Region: ")
     arrival = input("Enter Arrival: ")
-    
+
     mode(opt1)
+
     SendKeys('{TAB}' + '2' + '{ENTER}')
     SendKeys(region + '{ENTER 2}')
     SendKeys(arrival + '{ENTER}')
@@ -297,15 +295,16 @@ def check_dates():
 def check_tours():
     office = input("Enter Office: ")
     starting = input("Enter Tour Date: ")
-    
+
     mode(opt1)
+
     SendKeys('{TAB}' + '3' + '{ENTER}')
     SendKeys(office + '{ENTER}')
     SendKeys(starting + '{ENTER 3}')
     SendKeys('{TAB}' + '2' + '{ENTER}')
     main()
 
-def check_cashback(leadn,loca):
+def check_cashback(leadn, loca):
     mode(opt3)
 
     SendKeys(leadn + '{ENTER}')
@@ -317,40 +316,40 @@ def check_cashback(leadn,loca):
     SendKeys('18' + '{ENTER}')
     time.sleep(1)
     SendKeys('{BACKSPACE 3}')
-    main()    
+    main()
 
 #Build A Package
 def build_package():
     mode(opt1)
     #Check if Exist first
     SendKeys("{ENTER}")
-    lastName = input("Enter Last Name: ")
-    FirstName = input("Enter First Name: ")
-    PhoneNo = input("Phone Number: ")
-    phoneNo2 = input("Second Phone Number: ")
-    Country = input("Enter Country Code: ")
+    last_name = input("Enter Last Name: ")
+    first_name = input("Enter First Name: ")
+    phone_no = input("Phone Number: ")
+    phone_no2 = input("Second Phone Number: ")
+    country = input("Enter Country Code: ")
     addr = input("Address: ")
-    zipCode = input("Input ZipCode: ")
+    zip_code = input("Input Zip Code: ")
     hhn = input("Hilton Honors Number: ")
     email = input("Enter Email: ")
     lang = input("Preferred Language: ")
     natio = input("Nationality: ")
-    
+
     set_window()
-    
-    #LastName,FirstName,Phone Number
-    SendKeys(lastName.replace(" ","{SPACE}") + "{ENTER}")
-    SendKeys(FirstName.replace(" ","{SPACE}") + "{ENTER}")
-    SendKeys(PhoneNo + "{ENTER 2}")
-    #Country,Enter*4
-    if "/" in lastName or "/" in FirstName:
-        SendKeys(Country + "{ENTER 5}")
+
+    #last_name,first_name,Phone Number
+    SendKeys(last_name.replace(" ", "{SPACE}") + "{ENTER}")
+    SendKeys(first_name.replace(" ", "{SPACE}") + "{ENTER}")
+    SendKeys(phone_no + "{ENTER 2}")
+    #country,Enter*4
+    if "/" in last_name or "/" in first_name:
+        SendKeys(country + "{ENTER 5}")
     else:
-        SendKeys(Country + "{ENTER 4}")
-    SendKeys(zipCode + "{ENTER}")
-    SendKeys(addr.replace(" ","{SPACE}") + "{ENTER 5}")
+        SendKeys(country + "{ENTER 4}")
+    SendKeys(zip_code + "{ENTER}")
+    SendKeys(addr.replace(" ", "{SPACE}") + "{ENTER 5}")
     time.sleep(2)
-    
+
     #Check if address good
     addrgood = input("Select Option: override continue ")
 
@@ -359,13 +358,14 @@ def build_package():
         SendKeys("o" + "{ENTER}")
     elif addr == "c":
         pass
-    
-    SendKeys("{ENTER 9}")    
+
+    SendKeys(phone_no2)
+    SendKeys("{ENTER 9}")
     SendKeys(hhn + "{ENTER 2}")
     SendKeys(email + "{ENTER 2}")
     SendKeys(lang + "{ENTER}")
     SendKeys(natio + "{ENTER 2}")
-    
+
     ###Marketing Key Section
     ##Check Department
     dptm = input("Department outbnd, orl, gldmtn, lvn: ")
@@ -375,28 +375,28 @@ def build_package():
     stt = input("Marital Status: ")
     tsr = input("Agent tsr: ")
     if dptm == "outbnd":
-        keycodes(4,3,12,outbnd_orl)
-        assign_key(4,outbnd_orl)
-        mktkey = outbnd_call_trsf[key_loca]
+        keycodes(4, 3, 12, outbnd_orl)
+        assign_key(4, outbnd_orl)
+        mktkey = outbnd_orl[key_loca]
     if dptm == "orl":
-        keycodes(18,33,27,orl_call_trsf)
-        assign_key(18,orl_call_trsf)
+        keycodes(18, 33, 27, orl_call_trsf)
+        assign_key(18, orl_call_trsf)
         mktkey = orl_call_trsf[key_loca]
     if dptm == "gldmtn":
-        keycodes(46,33,55,gldmtn_call_trsf)
-        assign_key(46,gldmtn_call_trsf)
+        keycodes(46, 33, 55, gldmtn_call_trsf)
+        assign_key(46, gldmtn_call_trsf)
         mktkey = gldmtn_call_trsf[key_loca]
     if dptm == "lvn":
-        keycodes(32,33,41,lvn_call_trsf)
-        assign_key(32,lvn_call_trsf)
+        keycodes(32, 33, 41, lvn_call_trsf)
+        assign_key(32, lvn_call_trsf)
         mktkey = lvn_call_trsf[key_loca]
-    
+
     set_window()
     SendKeys(mktkey + "{ENTER}")
     SendKeys("{BACKSPACE}")
     SendKeys("{ENTER 2}")
     SendKeys(promo + "{ENTER 6}")
-    
+
     #Payment
     SendKeys(pckgcode + "{ENTER 5}")
     price = input("Price: ")
@@ -408,7 +408,7 @@ def build_package():
 
     if not ccgood:
         main()
-        
+
     set_window()
     SendKeys("{ENTER 2}")
     SendKeys(str(tsr) + "{ENTER}")
@@ -418,8 +418,7 @@ def build_package():
     SendKeys("1" + "{ENTER}")
     SendKeys("1" + "{ENTER}")
     SendKeys(mktkey + "{ENTER 3}")
-    SendKeys(stta + "{ENTER 10}")
+    SendKeys(stt + "{ENTER 10}")
     main()
 
 main()
-
