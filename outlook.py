c162637e1b2499e2a9b6804fdfa7996e3274f0bb
springdom <springdom@github.com>
2017-08-13 00:32:51 -0400

    a

--- outlook.py
+++ outlook.py
@@ -6,19 +6,19 @@ outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 # "6" refers to the index of a folder - in this case,
 # the inbox. You can change that number to reference
 # any other folder
-inbox = outlook.GetDefaultFolder(6) 
-
-messages = inbox.Items
+#inbox = outlook.GetDefaultFolder(6) 
+mebox = outlook.Folders['Inbox']
+messages = mebox.Items
 message = messages.GetFirst()
 rec_time = message.CreationTime
 body_content = message.body
 subj_line = message.subject
 
-print(message.subject, message.CreationTime)
-print(body_content)
+#print(message.subject, message.CreationTime)
+#print(body_content)
 
-"""
+
 while message:
     print(message.subject, message.CreationTime)
     message = messages.GetNext()
-"""
+
