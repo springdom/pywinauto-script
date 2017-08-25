15e7730cf66974af2e01a155d8e35f2a96f4c3d8
springdom <springdom@github.com>
2017-08-25 00:52:15 -0400

    add workgroups

--- CreateUser.py
+++ CreateUser.py
@@ -16,7 +16,6 @@ typein = app.dlg.type_keys
 #LAS,ORL,SPG
 #CT,ACT,CC,Outbound
 
-
 wb = load_workbook('orgchart.xlsx', data_only=True)
 sh = wb['HGV_OrgChart']
 ws = wb.active
@@ -27,34 +26,49 @@ orl_ct = ["CT Priority 1", "CT Priority 2", "LOC-ORL-MKT-HRCC", "MKT-InbCT-Callb
 orl_act = ["LOC-ORL-MKT-ACT", "MKT-Activations-CallBack", "MKT-ACT-Main", "MKT-CC-BookDates", "MKT-CC-BookDates-Priority1", "MKT-CC-CustomerService", "MKT-CC-CustomerService-Priority2"]
 orl_cc = ["LOC-ORL-MKT-CC",  "MKT-CC-BookDates", "MKT-CC-BookDates-Priority1", "MKT-CC-CustomerService", "MKT-CC-CustomerService-Priority2"]
 
-a = []
+licenses = ["Interaction Optimizer Client Access","Interaction Optimizer Real-time Adherence Tracking","Interaction Optimizer Schedulable"]
+roles = ["MKT-Agent","MKT-SF-Agent","MKT-CC-Agent"]
 
 def main():
     pass
 
 def orgchart_data():
     n = 2
-    
-    
+#Find Correct columns
     for x in range(1,200):
         if sh['A%s' % n].value == "Add":
             username = sh['B%s' % n].value
             agentName = sh['D%s' % n].value
             tsr = str(sh['E%s' % n].value)
+            
             print(username, agentName, tsr)
+            
             Config(tsr, username)
             GetUserDetails(agentName)
             AutoACD()
             Roles("dept")
-            AgentWorkGroups("orl","ct",orl_ct)
+            getWorkGroups()
             Licensing("Test")
-            app.dlg.Cancel.click_input()
+            app.dlg.Cancel.click_input() #Change When DOne
             n += 1
-    
+
+def getWorkGroups():
+    if location == "orl":
+        if department == "ct":
+            AgentWorkGroups("orl", orl_ct)
+        if department == "act":
+            AgentWorkGroups("orl", orl_act)
+        if department == "cc":
+            AgentWorkGroups("orl", orl_cc)
+    elif location == "spg":
+        pass
+    elif location == "lvn":
+        pass
+
 def Config(tsr, win_username):
     app.dlg.menu_select("File->New")
     app.dlg.Edit0.type_keys(tsr + "{ENTER}")
-    
+
     #app.dlg.Edit.type_keys("Matthew") #Extension
     app.dlg.Edit3.type_keys("10102015") #Password
     app.dlg.Edit4.type_keys("10102015") #Confirm Password
@@ -65,25 +79,31 @@ def Config(tsr, win_username):
 
 def GetUserDetails(agentName):
     name = agentName.split(" ")
-    
+
     app.dlg.TabItem3.click_input()
     app.dlg.Edit0.type_keys(name[0]) #FirstName
     app.dlg.Edit2.type_keys(name[1]) #LastName
     app.dlg.Edit4.type_keys(name[0] + "{SPACE}" + name[1]) #Display Name
 
-def AgentWorkGroups(loc,deptmnt,wrkgrps):
+
+def AgentWorkGroups(loc,wrkgrps):
     num = 0
     app.dlg.Workgroups.click_input()
     app.dlg.OK.click_input()
+    
     for x in wrkgrps:
         num+=1
         app.dlg[x].click_input()
         app.dlg.Add.click_input()
         if num == 3:
             for x in range(1,4):
-                app.dlg['VerticalScrollBar'].click_input()
+                app.dlg['VerticalScrollBar'].click_input() #Click Scroll button insteal scrollbar
+
+
+
 
 def Queues():
+    pass
     
                               
 def AutoACD():
@@ -91,7 +111,7 @@ def AutoACD():
     app.dlg.ListItem3.click_input()
     app.dlg.CheckBox0.click_input() #Auto ACD
 
-def Roles(dept):
+def Roles(deptmnt):
     app.dlg.Roles.click_input()
     app.dlg.Add.click_input()
     app.dlg.ListItem4.select()
@@ -101,10 +121,17 @@ def Licensing(department):
     app.dlg.Licensing.click_input()
     app.dlg.OK.click_input()
     app.dlg['Enable Licenses'].click_input()
+    for ls in licenses:
+        app.dlg[ls].type_keys("{SPACE}")
+        
+    """"
     app.dlg['Interaction Optimizer Client Access'].type_keys("{SPACE}")
     app.dlg['Interaction Optimizer Real-time Adherence Tracking'].type_keys("{SPACE}")
     app.dlg['Interaction Optimizer Schedulable'].type_keys("{SPACE}")
+    """
 
+location = input("Location orl, spg, lvn: ")
+department = input("Department ct, act, cc:")
 
 orgchart_data()
 
@@ -116,10 +143,12 @@ Agent Queues
 No Licenses
 Workgroups - MKT-Outbound-Callback, MKT-Outbound-Main2, Orl_OUT_SUP
 Roles - MKT-Agent
+
 - Las Vegas - OutBoundCMS
 No Licenses
 Workgroups - MKT-Outbound-Callback, MKT-Outbound-Main2, LV_OUT_SUP
 Roles - MKT-Agent
+
 - SpringField - OutBoundCMS
 No Licenses
 Workgroups - MKT-Outbound-Callback, MKT-Outbound-Main2, SPG_OUT_SUP
@@ -129,10 +158,12 @@ Roles - MKT-Agent
 No Licenses
 LOC-ORL-MKT-SalesForce, SF-Orlando-Manual, SF-RestrictDialing
 Roles - MKT-SF-Agent
+
 - Las Vegas - OutBoundManual
 No Licenses
 LOC-LV-MKT-SalesForce, SF-LV-Manual, SF-RestrictDialing
 Roles - MKT-SF-Agent
+
 - SpringField - OutBoundManual
 No Licenses
 LOC-SPG-MKT-SalesForce, SF-Springfield-Manual, SF-RestrictDialing
@@ -142,14 +173,17 @@ Roles - MKT-SF-Agent
 Enable Licenses - Interaction Optimizer CLient Access, Interaction Optimizer Real Time Adherance Tracking, Interaction Optimizer Scheduable
 Workgroups - CT Priority 1, CT Priority 2, LOC-ORL-MKT-HRCC, MKT-InbCT-Callback, MKT-InbCT-HRCC
 Roles - MKT-Agent
+
 - Las Vegas - Call Transfer
 Enable Licenses - Interaction Optimizer CLient Access, Interaction Optimizer Real Time Adherance Tracking, Interaction Optimizer Scheduable
 Workgroups - CT Priority 1, CT Priority 2, LOC-LV-MKT-HRCC, MKT-InbCT-Callback, MKT-InbCT-HRCC
 Roles - MKT-Agent
+
 - SpringField - Call Transfer
 Enable Licenses - Interaction Optimizer CLient Access, Interaction Optimizer Real Time Adherance Tracking, Interaction Optimizer Scheduable
 Workgroups - LOC-SPG-MKT-HRCC, MKT-InbCT-Callback, MKT-InbCT-HRCC
 Roles - MKT-Agent
+
 Call Transfer - Client Optimizer
 ---------------------------------------------------------------------
 - Orlando Activations
