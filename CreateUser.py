3b9fd3713401ec20bc20e60e667bdfc1c606cf36
springdom <springdom@github.com>
2017-08-30 00:14:04 -0400

    acc

--- CreateUser.py
+++ CreateUser.py
@@ -14,6 +14,29 @@ from string import ascii_lowercase
 serv = input("1:CMS\n2:Salesforce\nSelect Option: ")
 serv = int(serv)
 
+#CMS
+orl_outbnd_cms = ["MKT-Outbound-Callback", "MKT-Outbound-Main2", "Orl_OUT_SUP"]
+orl_ct = ["CT Priority 1", "CT Priority 2", "LOC-ORL-MKT-HRCC", "MKT-InbCT-Callback", "MKT-InbCT-HRCC"]
+orl_act = ["LOC-ORL-MKT-ACT", "MKT-Activations-CallBack", "MKT-ACT-Main", "MKT-CC-BookDates", "MKT-CC-BookDates-Priority1", "MKT-CC-CustomerService", "MKT-CC-CustomerService-Priority2"]
+orl_cc = ["LOC-ORL-MKT-CC", "MKT-CC-BookDates", "MKT-CC-BookDates-Priority1", "MKT-CC-CustomerService", "MKT-CC-CustomerService-Priority2"]
+spg_ct = ["LOC-SPG-MKT-HRCC", "MKT-InbCT-Callback", "MKT-InbCT-HRCC"]
+lvn_outbnd_cms = ["LAS_OUT_SUP","MKT-Outbound-Callback", "MKT-Outbound-Main2"]
+lv_ct = ["CT Priority 1", "CT Priority 2", "LOC-LV-MKT-HRCC", "MKT-InbCT-Callback", "MKT-InbCT-HRCC"]
+wrkqueues = {"orl_outbnd_cms":orl_outbnd_cms,"orl_ct":orl_ct,"orl_act":orl_act,"orl_cc":orl_cc,"spg_ct":spg_ct,"lvn_outbnd_cms":lvn_outbnd_cms,"lvn_ct":lv_ct}
+
+#SalesForce
+orl_outbnd_sf = ["LOC-ORL-MKT-SalesForce", "SF-Orlando-Manual", "SF-RestrictDialing"] #Manual
+spg_outbnd_sf = ["LOC-SPG-MKT-SalesForce", "SF-Springfield-Manual", "SF-RestrictDialing"]
+lvn_outbnd_sf = ["LOC-LAS-MKT-SalesForce", "SF-Vegas-Manual", "SF-RestrictDialing"]
+
+licenses = ["Interaction Optimizer Client Access", "Interaction Optimizer Real-time Adherence Tracking", "Interaction Optimizer Schedulable"]
+roles = ["MKT-Agent", "MKT-SF-Agent", "MKT-CC-Agent"]
+
+wb = load_workbook('orgchart.xlsx', read_only=True)
+sh = wb['HGV_OrgChart']
+ws = wb.active
+
+column_header = {}
 app = Application(backend='uia')
 if serv == 1:
     p = pywinauto.findwindows.find_element(title="Interaction Administrator - [HiltonACD]")
@@ -28,39 +51,17 @@ typein = app.dlg.type_keys
 
 #app.dlg.print_control_identifiers() #Check Identifiers
 
-wb = load_workbook('orgchart.xlsx', read_only=True)
-sh = wb['HGV_OrgChart']
-ws = wb.active
-
-#CMS
-orl_outbnd_cms = ["MKT-Outbound-Callback", "MKT-Outbound-Main2", "Orl_OUT_SUP"]
-orl_ct = ["CT Priority 1", "CT Priority 2", "LOC-ORL-MKT-HRCC", "MKT-InbCT-Callback", "MKT-InbCT-HRCC"]
-orl_act = ["LOC-ORL-MKT-ACT", "MKT-Activations-CallBack", "MKT-ACT-Main", "MKT-CC-BookDates", "MKT-CC-BookDates-Priority1", "MKT-CC-CustomerService", "MKT-CC-CustomerService-Priority2"]
-orl_cc = ["LOC-ORL-MKT-CC", "MKT-CC-BookDates", "MKT-CC-BookDates-Priority1", "MKT-CC-CustomerService", "MKT-CC-CustomerService-Priority2"]
-spg_ct = ["LOC-SPG-MKT-HRCC", "MKT-InbCT-Callback", "MKT-InbCT-HRCC"]
-lv_outbnd_cms = ["LAS_OUT_SUP","MKT-Outbound-Callback", "MKT-Outbound-Main2"]
-lv_ct = ["CT Priority 1", "CT Priority 2", "LOC-LV-MKT-HRCC", "MKT-InbCT-Callback", "MKT-InbCT-HRCC"]
-
-#SalesForce
-orl_outbnd_sf = ["LOC-ORL-MKT-SalesForce", "SF-Orlando-Manual", "SF-RestrictDialing"] #Manual
-spg_outbnd_sf = ["LOC-SPG-MKT-SalesForce", "SF-Springfield-Manual", "SF-RestrictDialing"]
-lv_outbnd_sf = ["LOC-LAS-MKT-SalesForce", "SF-Vegas-Manual", "SF-RestrictDialing"]
-
-licenses = ["Interaction Optimizer Client Access", "Interaction Optimizer Real-time Adherence Tracking", "Interaction Optimizer Schedulable"]
-roles = ["MKT-Agent", "MKT-SF-Agent", "MKT-CC-Agent"]
-
-column_header = {}
-Add = "Add/Delete/Change/Transfer/Rehire"
-Add2 = "Add/Delete/Change"
-windows = "Windows for Adds"
-email = "Email Address"
-Name = "AgentName"
-cic_id = "CIC_ID"
-
-
 location = input("Location orl, spg, lvn: ")
+location = location.lower()
+
 if serv == 1:
-    department = input("Department outbnd, ct, act, cc: ")
+    if location == "spg":
+        department = input("Select a department - ct:  ")
+    if location == "lvn":
+        department = input("Select a department - outbnd, ct: ")
+    else:
+        department = input("Select a department - outbnd, ct, act, cc: ")
+    department = department.lower()
 
 def main():
     get_alphabet()
@@ -78,70 +79,56 @@ def sed(a):
             return str(k)
 
 def column_headers():
+    Add = "Add/Delete/Change/Transfer/Rehire"
+    Add2 = "Add/Delete/Change"
+    windows = "Windows for Adds"
+    email = "Email Address"
+    Name = "AgentName"
+    cic_id = "CIC_ID"
+    
     add = sed(Add) or sed(Add2)
     agent_username = sed(windows)
-    agentemail = sed(email) or "AZ"
     agent_name = sed(Name)
     agent_tsr = sed(cic_id)
 
-    orgchart_data(add, agent_username, agentemail,agent_name, agent_tsr)
+    orgchart_data(add, agent_username,agent_name, agent_tsr)
 
-def orgchart_data(add, windows, agent_email, agent_name, agent_tsr):
+def orgchart_data(add, windows, agent_name, agent_tsr):
     n = 2
     while n < sh.max_row:
         if sh[add + str(n)].value == "Add":
             username = sh[windows + str(n)].value
             agentName = sh[agent_name + str(n)].value
-            email = sh[agent_email + str(n)].value
             tsr = sh[agent_tsr + str(n)].value
 
-            print(username, email, agentName, tsr)
+            print(username, agentName, tsr)
 
             Config(tsr, username)
             GetUserDetails(agentName)
             AutoACD()
+            
             if serv == 1:
                 Roles(department)
             else:
                 SFRoles()
             if serv == 1:
-                getWorkGroups()
+                getWorkGroups(location, department)
                 if department == "ct" or department == "cc":
                     Licensing()
             else:
                 getSFWorkGroups()
-            #Email
-            """
-            if loc == "orl":
-                Email(email)
-            """
-            app.dlg.Cancel.click_input() #Change When DOne
+            app.dlg.Cancel.click_input() #Change When Done
         n += 1
 
-def getWorkGroups(): # helper function here
-    if location == "orl":
-        if department == "outbnd":
-            AgentWorkGroups(orl_outbnd_cms)
-        if department == "ct":
-            AgentWorkGroups(orl_ct)
-        if department == "act":
-            AgentWorkGroups(orl_act)
-        if department == "cc":
-            AgentWorkGroups(orl_cc)
-
-    if location == "spg":
-        if department == "outbnd":
-            AgentWorkGroups(spg_outbnd_cms)
-        if department == "ct":
-            AgentWorkGroups(spg_ct)
-
-    if location == "lvn":
-        if department == "outbnd":
-            AgentWorkGroups(lv_outbnd_cms)
-        if department == "ct":
-            AgentWorkGroups(lv_ct)
-
-def getSFWorkGroups(): # helper function here
+def getWorkGroups(loc, dept):
+    if location == loc:
+        if department == dept:
+            if department == "outbnd":
+                AgentWorkGroups(wrkqueues[loc + "_" + dept + "_" + "cms"])
+            else:
+                AgentWorkGroups(wrkqueues[loc + "_" + dept])
+    
+def getSFWorkGroups(): #helper function here
     if location == "orl":
         AgentSFWorkGroups(orl_outbnd_sf)
     if location == "spg":
@@ -149,18 +136,30 @@ def getSFWorkGroups(): # helper function here
     if location == "lvn":
         AgentSFWorkGroups(lv_outbnd_sf)
 
-def Config(tsr, win_username):
-    app.dlg.menu_select("File->New")
+def Config(tsr, win_username): #Check If Already Exist
+    static = app.DialogName.child_window(title_re='.*Please contact your system administrator.',
+                                     class_name_re='Static')
+    """
+    if static.exists(timeout=20): # if it opens no later than 20 sec.
+        app.DialogName.OK.click()
+    """
+    app.dlg.type_keys('^n')
     app.dlg.Edit0.type_keys(str(tsr) + "{ENTER}")
+        
 
     app.dlg.Edit3.type_keys("10102015") #Password
     app.dlg.Edit4.type_keys("10102015") #Confirm Password
     app.dlg.Edit7.type_keys("hgvcnt\\" + win_username) #Domain User
-    #Location?
-    """ if orlando, las vegas, springfield
+    getLoc()
+
+def getLoc():
     app.dlg["ComboBox4"].click_input()
-    app.dlg["Las Vegas"].select()
-    """
+    if location == "orl":
+        app.dlg["Orlando - Metro Center"].select()
+    if location == "spg":
+        app.dlg["Spingfield"].select()
+    if location == "lvn":
+        app.dlg["Las Vegas"].select()
 
 #Personal Info
 def GetUserDetails(agentName):
@@ -170,6 +169,7 @@ def GetUserDetails(agentName):
     app.dlg.Edit0.type_keys(name[0]) #FirstName
     app.dlg.Edit2.type_keys(name[1]) #LastName
     app.dlg.Edit4.type_keys(name[0] + "{SPACE}" + name[1]) #Display Name
+
 
 def AgentWorkGroups(wrkgrps):
     num = 0
@@ -263,7 +263,6 @@ def Roles(deptmnt):
     elif deptmnt == "act" or deptmnt == "cc":
         app.dlg.ListItem6.select()
 
-
     app.dlg.OK.click_input()
 
 def SFRoles():
@@ -278,16 +277,8 @@ def Licensing():
     app.dlg['Enable Licenses'].click_input()
     for ls in licenses:
         app.dlg[ls].type_keys("{SPACE}")
-
-def Email(email):
-    app.dlg['Configuration'].click_input()
-    app.dlg['...'].click_input()
-    app.dlg['RadioButton6'].click_input()
-    app.dlg['Email address'].click_input()
-    app.dlg["Edit"].type_keys("{BS 40}")
-    app.dlg["Edit"].type_keys(email)
-    
-#main()
+   
+main()
 
 """
 Agent Queues
