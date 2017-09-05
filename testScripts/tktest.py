8e27700c42f4c5e16287deeacf300ca37a5af863
springdom <springdom@github.com>
2017-09-04 22:29:41 -0400

    add excel to tkint

--- testScripts/tktest.py
+++ testScripts/tktest.py
@@ -1,13 +1,59 @@
 from openpyxl import load_workbook
 from string import ascii_lowercase
-import os
-import sys
 from tkinter import Tk, Label, Radiobutton,Button, W, NORMAL,DISABLED, StringVar
+
+
+wb = load_workbook('orgchart.xlsx', read_only=True)
+sh = wb.worksheets[0]
+ws = wb.active
+column_header = {}
+#Used Columns
+Add = "Add/Delete/Change/Transfer/Rehire"
+Add2 = "Add/Delete/Change"
+Add3 = "Add/Delete/Change/Transfer"
+windows = "Windows for Adds"
+email = "Email Address"
+Name = "AgentName"
+cic_id = "CIC_ID"
+
+
+#loop through and find column name
+for c in ascii_lowercase:
+	x = sh[c.upper() + "1"].value
+	column_header[c.upper()] = sh[c.upper() + "1"].value
+
+def orgchart_data(add, windows, agent_email,agent_name, agent_tsr):
+    n = 2
+    while n < sh.max_row :
+        if sh[add + str(n)].value == "Add":
+            username = sh[windows + str(n)].value
+            agentName = sh[agent_name + str(n)].value
+            email = sh[agent_email + str(n)].value
+            tsr = sh[agent_tsr + str(n)].value
+            
+            print(username, email,agentName, tsr)
+        n += 1
+
+def sed(a):
+     for k,v in column_header.items():
+         if v == a:
+             a = k
+             return str(k)
+
+def cheese():
+    add = sed(Add) or sed(Add2) or sed(Add3)
+    agent_username = sed(windows)
+    agentemail = sed(email) or "AZ"
+                                   
+    agent_name =  sed(Name)
+    agent_tsr = sed(cic_id)
+  
+    orgchart_data(add, agent_username, agentemail,agent_name, agent_tsr)
 
 
 class IAAutoGUI:
     def __init__(self, master, server = 0):
-        self.server = server        
+        self.server = server
         self.master = master
         master.title("IA Auto")
         self.master.minsize(width=300,height=200)
@@ -86,7 +132,6 @@ class IAAutoGUI:
     
     def get_location(self, location):
         self.location = location
-        #self.server = server
         
         print(location)
         self.orl_button.configure(state=DISABLED)
@@ -118,16 +163,16 @@ class IAAutoGUI:
         self.cc.configure(state = DISABLED)
         self.act.configure(state = DISABLED)
         self.cc.configure(state = DISABLED)
-
-    def restart_button(self):
-        """Restarts the current program.
-        Note: this function does not return. Any cleanup action (like
-        saving data) must be done before calling this function."""
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
+        cheese()
         
-            
+    def restart_button(self):
+        pass
 
-root = Tk()
-my_gui = IAAutoGUI(root)
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    my_gui = IAAutoGUI(root)
+    root.mainloop()
+            
+cheese()
+ 
+
